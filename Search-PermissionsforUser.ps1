5c9da16a239e42e6b7214e50d70d90a3da195b19
Ainsey11 <robert@ainsey11.com>
2016-04-12 16:25:14 +0100

    It's a start

---
+++ Search-PermissionsforUser.ps1
@@ -0,0 +1,32 @@
+﻿#Prompt for input (username, root folder)
+$User = Read-Host "Enter the username to check"
+$RootDir = Read-Host "Enter the root directory to check"
+#Get list of subdirectories. We just need the Fullname (Complete path).
+$Dirs = @(gci $RootDir -recurse) | Where {$_.mode -match "d"} | Select Fullname
+#Create array to store results
+$Compiled = @()
+forEach ($Dir in $dirs)
+{
+   #Get the ACL for the current subdirectory. Format it in a way we can work with.
+    $ACL = Get-Acl $dir.fullname
+    $List = @($ACL.access | Select IdentityReference,AccessControlType,FileSystemRights)
+   #Filter for $user, remove List and ACL arrays
+    $Filtered = $List | Where {$_.identityreference -match $user} 
+    Remove-item variable:ACL
+    Remove-item variable:List
+    
+   #Create an object for each line in the above $List.
+    forEach ($line in $Filtered)
+    {
+        $Object = New-Object PSObject -property @{
+            "Directory" = $dir.Fullname
+            "User" = $line.IdentityReference
+            "AccessControl" = $line.AccessControlType
+            "Rights" = $line.FileSystemRights
+            }
+       #Add this object to the $Compiled array.    
+        $Compiled += $Object
+    }
+}
+#Save the report.  You could add a Read-Host statement here to prompt for a filename/location, if you want.
+$Compiled | Select Directory,user,accesscontrol,rights | Sort Directory | Export-CSV -notypeinformation "$env:userprofile\desktop\Permissions_$user.csv"
---
+++ Send-PermissionsAlert.ps1
@@ -0,0 +1,19 @@
+﻿############################################################
+# Searches Folder structure for files and folders that
+# have individual permissions set, which is bad juju
+#
+# Author : Robert Ainsworth
+# Web : https://ainsey11.com
+###########################################################
+
+# Defining my variables, because y'know - variables are handy
+
+
+#Set the file server you want to scan. if using DFSR then select one of the nodes, ntf permissions are replicated.
+$FileServer = "" 
+
+#Put the top of the folder structure here, for example D:\Share the script will recursively scan anything beneath this.
+$FolderLocation = ""
+
+
+Get-Acl -Path "S:\Group\IT" | select -ExpandProperty Access
