8061614768afa2d2d3dc0581ca972ca38f9cd068
Ainsey11 <robert@ainsey11.com>
2016-05-06 15:17:14 +0100

    Got rid of the old one, starting over

--- Search-PermissionsforUser.ps1
+++ Search-PermissionsforUser.ps1
@@ -1,32 +1,31 @@
-﻿#Prompt for input (username, root folder)
-$User = Read-Host "Enter the username to check"
-$RootDir = Read-Host "Enter the root directory to check"
-#Get list of subdirectories. We just need the Fullname (Complete path).
-$Dirs = @(gci $RootDir -recurse) | Where {$_.mode -match "d"} | Select Fullname
-#Create array to store results
-$Compiled = @()
-forEach ($Dir in $dirs)
-{
-   #Get the ACL for the current subdirectory. Format it in a way we can work with.
-    $ACL = Get-Acl $dir.fullname
-    $List = @($ACL.access | Select IdentityReference,AccessControlType,FileSystemRights)
-   #Filter for $user, remove List and ACL arrays
-    $Filtered = $List | Where {$_.identityreference -match $user} 
-    Remove-item variable:ACL
-    Remove-item variable:List
-    
-   #Create an object for each line in the above $List.
-    forEach ($line in $Filtered)
-    {
-        $Object = New-Object PSObject -property @{
-            "Directory" = $dir.Fullname
-            "User" = $line.IdentityReference
-            "AccessControl" = $line.AccessControlType
-            "Rights" = $line.FileSystemRights
-            }
-       #Add this object to the $Compiled array.    
-        $Compiled += $Object
+﻿############################
+# Name : Get-NonGroupPermissions.ps1
+# Function: Gets a list of folders within a share that have individual user permissions set, 
+# rather than the approved role groups from within AD
+#
+# Author - Robert Ainsworth - https://ainsey11.com
+
+
+# Setting variables
+$EmailFrom = "thq-man01@timico.co.uk"
+$EmailTo = "rob@timico.co.uk"
+$EmailServer = "mail.timicogroup.local"
+$EmailSubjectLine = "Files and Folders found with individual permissions assigned"
+$EMailBody
+
+
+$folders = "D:\Documents\github\"
+
+foreach ($path in $paths){   
+    gci $Paths | get-acl  | %{
+    $Acl=$_
+    $_.Access|?{$_.AccessControlType -eq "Allow"}|Select -Unique IdentityReference|%{
+        [PSCustomObject]@{
+        "Path"=$ACL.PSPath.substring(38,$Acl.PSPath.Length-38); 
+        "Owner"=$ACL.Owner;
+        "Group"=$ACL.Group;
+        "Access"=$_.IdentityReference | Where-Object {$_ -like "*$Users*"} 
+        }
     }
+} 
 }
-#Save the report.  You could add a Read-Host statement here to prompt for a filename/location, if you want.
-$Compiled | Select Directory,user,accesscontrol,rights | Sort Directory | Export-CSV -notypeinformation "$env:userprofile\desktop\Permissions_$user.csv"
